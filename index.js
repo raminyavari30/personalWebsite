47b5c0313097e9c496434beb0835ce39130d2917
Ramin <ramin_yavari03@yahoo.com>
2016-09-25 00:56:17 -0700

    Code Cleanup

--- index.js
+++ index.js
@@ -1,36 +1,39 @@
-/* Toggle between adding and removing the "active" and "show" classes when the user clicks on one of the "Section" buttons. 
-The "active" class is used to add a background color to the current button when its belonging panel is open. 
-The "show" class is used to open the specific accordion panel */
+  /* Toggle between adding and removing the "active" and "show" classes when the user clicks on one of the "Section" buttons. 
+  The "active" class is used to add a background color to the current button when its belonging panel is open. 
+  The "show" class is used to open the specific accordion panel */
 
-const acc = document.getElementsByClassName("accordion");
-
-for (let i = 0; i < acc.length; i++) {
-
-    acc[i].onclick = function(){
-      console.log(this)
-        this.classList.toggle("active");
-        this.nextElementSibling.classList.toggle("show");
-    }
+window.onload = () => {
+  init();
 }
 
-const devicons = document.getElementsByTagName("I");
+const init = () => {
 
-for(let i = 0; i < devicons.length; i++) {
+  const acc = document.getElementsByClassName("accordion");
 
-  devicons[i].onclick = function() {
+  for (let i = 0; i < acc.length; i++) {
 
-    let siblings = this.parentNode.children;
+      acc[i].onclick = function(){
+          this.classList.toggle("active");
+          this.nextElementSibling.classList.toggle("show");
+      }
+  }
 
-    for(let j = 0; j < siblings.length; j++) {
-      siblings[j].classList.toggle("technology");
+  const devicons = document.getElementsByTagName("I");
+
+  for(let i = 0; i < devicons.length; i++) {
+
+    devicons[i].onclick = function() {
+
+      let siblings = this.parentNode.children;
+
+      for(let j = 0; j < siblings.length; j++) {
+        siblings[j].classList.toggle("technology");
+      }
+      this.classList.toggle("technology");
+
+      let elementToShow = this.classList[0].split("-")[1];
+      
+      document.getElementById(elementToShow).classList.toggle("content");
     }
-    this.classList.toggle("technology");
-
-    var elementToShow = this.classList[0].split("-")[1];
-
-    console.log(document.getElementById(elementToShow).classList);
-    
-    document.getElementById(elementToShow).classList.toggle("content")
-
   }
 }
