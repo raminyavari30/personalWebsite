e7177f33919da7f1c3c82ca18891039d700008ab
Ramin <ramin_yavari03@yahoo.com>
2016-10-02 20:37:10 -0700

    Added more features to personal website

--- index.js
+++ index.js
@@ -38,15 +38,32 @@ var init = function() {
     });
   }
 
-var greetings = ["Hello!", "Hola!", "Salam!", "Namaste", "Ciao!"];
+  var greetings = ["Hello!", "Hola!", "Salam!", "Namaste", "Ciao!"];
 
-var greeting = document.getElementsByClassName("greeting");
-console.log(greeting);
-greeting[0].addEventListener("click", function() {
-  var choice = Math.floor(Math.random() * greetings.length);
+  var greeting = document.getElementsByClassName("greeting");
 
-  alert(greetings[choice]);
+  greeting[0].addEventListener("click", function() {
 
-  this.innerHTML = greetings[choice];
-});
+    var choice = Math.floor(Math.random() * greetings.length);
+
+    alert(greetings[choice]);
+
+    this.innerHTML = greetings[choice];
+  });
+
+  var quotes = ["Don't watch the clock; do what it does. Keep going.", "It always seems impossible until its done.", 
+  "Life is 10% what happens to you and 90% how you react to it.", 
+  "Try not to become a man of success, but rather try to become a man of value.", 
+  "Yesterday is not ours to recover, but tomorrow is ours to win or lose."];
+
+  setInterval(function() {
+
+    var quote = document.getElementsByTagName("H1");
+    
+    var randomQuote = Math.floor(Math.random() * quotes.length);
+
+    quote[0].innerHTML = quotes[randomQuote];
+  }, 5000);
 }
+
+
